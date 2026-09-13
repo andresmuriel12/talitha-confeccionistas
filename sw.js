23d3140d503e06f0a59fbd832fcd3555d3bc25b3
andresmuriel12 <carlos123elmore@gmail.com>
2026-09-12 22:29:36 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ================================================================
 // TALITHA CONFECCIONISTAS — Service Worker
 // ================================================================
-const CACHE_NAME    = 'talitha-conf-v8';
+const CACHE_NAME    = 'talitha-conf-v9';
 const STATIC_ASSETS = [
   '/', '/index.html', '/app.js', '/manifest.json',
   'https://cdn.tailwindcss.com',
